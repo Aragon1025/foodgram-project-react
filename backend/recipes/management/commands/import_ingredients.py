35b656ea090d76c421fcf697f640bc3cc0726ca3
Aragon <pavelnto10043@gmail.com>
2024-03-12 02:36:18 +0300

    test66

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -1,37 +1,21 @@
-import os
-from csv import reader
+import json
 
-from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+
 from recipes.models import Ingredient
-
-# Путь к папке с данными
-DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
 
 
 class Command(BaseCommand):
-    # Описание команды
-    help = 'Загружаем csv файл в базу данных'
-
-    # Определение аргументов команды
-    def add_arguments(self, parser):
-        parser.add_argument('filename', default='ingredients.csv', nargs='?',
-                            type=str)
-
-    # Логика выполнения команды
     def handle(self, *args, **options):
-        try:
-            # Открываем файл CSV и считываем его содержимое
-            with open(os.path.join(DATA_ROOT, options['filename']), 'r',
-                      encoding='utf-8') as file_csv:
-                data = reader(file_csv)
-                # Проходимся по строкам файла и создаем или обновляем записи
-                for name, measurement_unit in data:
-                    Ingredient.objects.get_or_create(
-                        name=name,
-                        measurement_unit=measurement_unit
-                    )
-        except FileNotFoundError:
-            # Обработка исключения, если файл не найден
-            msg = 'Добавьте файл c данными в директорию data'
-            raise CommandError(msg)
+
+        with open('data/ingredients.json', 'rb') as f:
+            data = json.load(f)
+        ingredients = []
+        for ingredient_data in data:
+            ingredients.append(
+                Ingredient(
+                    name=ingredient_data['name'],
+                    measurement_unit=ingredient_data['measurement_unit'],
+                )
+            )
+        Ingredient.objects.bulk_create(ingredients, ignore_conflicts=True)
