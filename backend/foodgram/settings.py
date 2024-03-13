4fd3f9840a2608dc9863c4646e121768f3f98e6f
Aragon <pavelnto10043@gmail.com>
2024-03-13 03:01:25 +0300

    test61

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -67,13 +67,13 @@ DATABASES = {
             'DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv(
             'POSTGRES_DB',
-            default='postgres'),
+            default='django'),
         'USER': os.getenv(
             'POSTGRES_USER',
-            default='postgres'),
+            default='django_user'),
         'PASSWORD': os.getenv(
             'POSTGRES_PASSWORD',
-            default='postgres'),
+            default='django_password'),
         'HOST': os.getenv(
             'DB_HOST',
             default='db'),
