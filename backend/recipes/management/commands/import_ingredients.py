e9e48db9c85c0af25e72130316b44caa26bd87b3
Aragon <pavelnto10043@gmail.com>
2024-03-14 03:52:03 +0300

    test66

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -3,12 +3,15 @@ from django.conf import settings
 from django.core.management import BaseCommand
 from recipes.models import Ingredient
 
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         file_path = str(settings.BASE_DIR / 'data' / 'ingredients.json')
         with open(file_path, 'r', encoding="utf8") as inp_f:
             reader = json.load(inp_f)
             for row in reader:
-                ingredient = Ingredient(name=row['name'], measurement_unit=row['measurement_unit'])
+                name = row['name']
+                measurement_unit = row['measurement_unit']
+                ingredient = Ingredient(name=name, measurement_unit=measurement_unit)
                 ingredient.save()
         self.stdout.write(self.style.SUCCESS('Successfully added ingredients'))
